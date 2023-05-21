97dd37d8a1b7b90467efb251829717a80b7985b5
Jacob Toomey <jtoomey@ucsb.edu>
2023-05-21 16:49:56 -0700

    Update RestaurantCreatePage.js

--- frontend/src/main/pages/Restaurants/RestaurantCreatePage.js
+++ frontend/src/main/pages/Restaurants/RestaurantCreatePage.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import { toast } from 'react-toastify';
 
 const createRestaurant = async (restaurant) => {
-  try {
+
     const response = await axios.post("/api/Restaurant/post", null, {
       params: {
         name: restaurant.name,
@@ -14,23 +14,19 @@ const createRestaurant = async (restaurant) => {
     });
 
     return response.data;
-  } catch (error) {
-    throw new Error(`Error creating restaurant: ${error.response.statusText}`);
-  }
+  
 };
 
 export default function RestaurantCreatePage() {
   let navigate = useNavigate();
 
   const onSubmit = async (restaurant) => {
-    try {
+    
       const createdRestaurant = await createRestaurant(restaurant);
       console.log("createdRestaurant: " + JSON.stringify(createdRestaurant));
       toast(`New restaurant Created - id: ${createdRestaurant.id} name: ${createdRestaurant.name}`);
       navigate("/restaurants");
-    } catch (error) {
-      console.error("Error creating restaurant: ", error);
-    }
+
   };
 
   return (
