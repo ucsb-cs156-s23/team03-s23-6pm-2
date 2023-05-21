82f429f4b1ec61b247c145aec6cc626ef12a9026
Jacob Toomey <jtoomey@ucsb.edu>
2023-05-20 21:28:42 -0700

    test fixes

--- frontend/src/tests/pages/Restaurants/RestaurantIndexPage.test.js
+++ frontend/src/tests/pages/Restaurants/RestaurantIndexPage.test.js
@@ -3,7 +3,11 @@ import RestaurantIndexPage from "main/pages/Restaurants/RestaurantIndexPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import mockConsole from "jest-mock-console";
-
+// for mocking /api/currentUser and /api/systemInfo
+import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";;
 const mockNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
@@ -40,7 +44,9 @@ jest.mock('main/utils/restaurantUtils', () => {
 
 
 describe("RestaurantIndexPage tests", () => {
-
+    const axiosMock =new AxiosMockAdapter(axios);
+    axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
     const queryClient = new QueryClient();
     test("renders without crashing", () => {
         render(
@@ -114,6 +120,6 @@ describe("RestaurantIndexPage tests", () => {
 
     });
 
-});
+});;
 
 
