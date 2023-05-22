06557443991bb22fb6574f455b6fd3b3c6894fce
Jacob Toomey <jtoomey@ucsb.edu>
2023-05-21 17:25:09 -0700

    Update RestaurantEditPage.test.js

--- frontend/src/tests/pages/Restaurants/RestaurantEditPage.test.js
+++ frontend/src/tests/pages/Restaurants/RestaurantEditPage.test.js
@@ -1,41 +1,39 @@
-import {fireEvent, render, screen, waitFor} from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import RestaurantEditPage from "main/pages/Restaurants/RestaurantEditPage";
-import {QueryClient, QueryClientProvider} from "react-query";
-import {MemoryRouter} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
 import AxiosMockAdapter from "axios-mock-adapter";
 import axios from "axios";
-import {apiCurrentUserFixtures} from "../../../fixtures/currentUserFixtures";
-import {systemInfoFixtures} from "../../../fixtures/systemInfoFixtures";
-
+import { apiCurrentUserFixtures } from "../../../fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "../../../fixtures/systemInfoFixtures";
 
 const mockToast = jest.fn();
-jest.mock('react-toastify', () => {
-  const originalModule = jest.requireActual('react-toastify');
+jest.mock("react-toastify", () => {
+  const originalModule = jest.requireActual("react-toastify");
   return {
     __esModule: true,
     ...originalModule,
-    toast: (x) => mockToast(x)
+    toast: (x) => mockToast(x),
   };
 });
 
 const mockNavigate = jest.fn();
-jest.mock('react-router-dom', () => {
-  const originalModule = jest.requireActual('react-router-dom');
+jest.mock("react-router-dom", () => {
+  const originalModule = jest.requireActual("react-router-dom");
   return {
     __esModule: true,
     ...originalModule,
     useParams: () => ({
-      id: 3
+      id: 3,
     }),
     Navigate: (x) => {
       mockNavigate(x);
       return null;
-    }
+    },
   };
 });
 
-describe("RestaurantEditPage tests", () => {
-
+describe("EditRestaurantPage tests", () => {
   const axiosMock = new AxiosMockAdapter(axios);
   let queryClient;
 
@@ -45,14 +43,15 @@ describe("RestaurantEditPage tests", () => {
     axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
     axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
   });
-  describe("when backend doesn't return a restaurant", () => {
-    test("renders header only", async () => {
-      axiosMock.onGet("/api/Restaurant", {params: {id: 3}}).timeout();
+
+  describe("when API does not return a restaurant", () => {
+    test("renders headline only", async () => {
+      axiosMock.onGet("/api/Restaurant", { params: { id: 3 } }).timeout();
 
       render(
         <QueryClientProvider client={queryClient}>
           <MemoryRouter>
-            <RestaurantEditPage/>
+            <RestaurantEditPage />
           </MemoryRouter>
         </QueryClientProvider>
       );
@@ -62,12 +61,12 @@ describe("RestaurantEditPage tests", () => {
     });
   });
 
-  describe("when backend is working", () => {
+  describe("functional API", () => {
     beforeEach(() => {
-      axiosMock.onGet("/api/Restaurant", {params: {id: 3}}).reply(200, {
+      axiosMock.onGet("/api/Restaurant", { params: { id: 3 } }).reply(200, {
         name: "Freebirds",
         description: "Burritos",
-        id: 3
+        id: 3,
       });
     });
 
@@ -75,17 +74,17 @@ describe("RestaurantEditPage tests", () => {
       render(
         <QueryClientProvider client={queryClient}>
           <MemoryRouter>
-            <RestaurantEditPage/>
+            <RestaurantEditPage />
           </MemoryRouter>
         </QueryClientProvider>
       );
     });
 
-    test("is populated with existing restaurant", async () => {
+    test("populates form with existing restaurant data", async () => {
       render(
         <QueryClientProvider client={queryClient}>
           <MemoryRouter>
-            <RestaurantEditPage/>
+            <RestaurantEditPage />
           </MemoryRouter>
         </QueryClientProvider>
       );
@@ -104,17 +103,17 @@ describe("RestaurantEditPage tests", () => {
       expect(updateButton).toBeInTheDocument();
     });
 
-    test("on submit, calls backend and redirects to index page", async () => {
+    test("on form submit, calls API and navigates to restaurant list", async () => {
       axiosMock.onPut("/api/Restaurant").reply(200, {
-        name: "Angrybirds",
-        description: "No more burritos",
-        id: "3"
+        name: "JONAHTNAN",
+        description: "Big buff",
+        id: "3",
       });
 
       render(
         <QueryClientProvider client={queryClient}>
           <MemoryRouter>
-            <RestaurantEditPage/>
+            <RestaurantEditPage />
           </MemoryRouter>
         </QueryClientProvider>
       );
@@ -132,25 +131,30 @@ describe("RestaurantEditPage tests", () => {
       expect(descriptionInput).toHaveValue("Burritos");
       expect(updateButton).toBeInTheDocument();
 
-      fireEvent.change(nameInput, {target: {value: 'Angrybirds'}});
-      fireEvent.change(descriptionInput, {target: {value: 'No more burritos'}});
+      fireEvent.change(nameInput, { target: { value: "JONAHTNAN" } });
+      fireEvent.change(descriptionInput, { target: { value: "Big buff" } });
 
       fireEvent.click(updateButton);
 
-      await waitFor(() => expect(mockToast).toHaveBeenCalledWith(
-        "Restaurant Updated - id: 3 name: Angrybirds"
-      ));
-      await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith({
-        to: "/restaurants"
-      }));
-
+      await waitFor(() =>
+        expect(mockToast).toHaveBeenCalledWith(
+          "Restaurant Updated - id: 3 name: JONAHTNAN"
+        )
+      );
+      await waitFor(() =>
+        expect(mockNavigate).toHaveBeenCalledWith({
+          to: "/restaurants",
+        })
+      );
 
       expect(axiosMock.history.put.length).toBe(1);
-      expect(axiosMock.history.put[0].params).toEqual({id: 3});
-      expect(axiosMock.history.put[0].data).toBe(JSON.stringify({
-        name: "Angrybirds",
-        description: "No more burritos",
-      }));
+      expect(axiosMock.history.put[0].params).toEqual({ id: 3 });
+      expect(axiosMock.history.put[0].data).toBe(
+        JSON.stringify({
+          name: "JONAHTNAN",
+          description: "Big buff",
+        })
+      );
     });
   });
 });
