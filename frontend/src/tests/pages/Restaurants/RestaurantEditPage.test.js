de8f23c5e40b45f7e5b75ab858e4c07fae85ca0f
Jacob Toomey <jtoomey@ucsb.edu>
2023-05-21 17:15:36 -0700

    Working edit

--- frontend/src/tests/pages/Restaurants/RestaurantEditPage.test.js
+++ frontend/src/tests/pages/Restaurants/RestaurantEditPage.test.js
@@ -1,117 +1,156 @@
-import { render, screen, act, waitFor, fireEvent } from "@testing-library/react";
+import {fireEvent, render, screen, waitFor} from "@testing-library/react";
 import RestaurantEditPage from "main/pages/Restaurants/RestaurantEditPage";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { MemoryRouter } from "react-router-dom";
-import mockConsole from "jest-mock-console";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {MemoryRouter} from "react-router-dom";
+import AxiosMockAdapter from "axios-mock-adapter";
+import axios from "axios";
+import {apiCurrentUserFixtures} from "../../../fixtures/currentUserFixtures";
+import {systemInfoFixtures} from "../../../fixtures/systemInfoFixtures";
 
-const mockNavigate = jest.fn();
 
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useParams: () => ({
-        id: 3
-    }),
-    useNavigate: () => mockNavigate
-}));
-
-const mockUpdate = jest.fn();
-jest.mock('main/utils/restaurantUtils', () => {
-    return {
-        __esModule: true,
-        restaurantUtils: {
-            update: (_restaurant) => {return mockUpdate();},
-            getById: (_id) => {
-                return {
-                    restaurant: {
-                        id: 3,
-                        name: "Freebirds",
-                        description: "Burritos"
-                    }
-                }
-            }
-        }
-    }
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+  const originalModule = jest.requireActual('react-toastify');
+  return {
+    __esModule: true,
+    ...originalModule,
+    toast: (x) => mockToast(x)
+  };
 });
 
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+  const originalModule = jest.requireActual('react-router-dom');
+  return {
+    __esModule: true,
+    ...originalModule,
+    useParams: () => ({
+      id: 3
+    }),
+    Navigate: (x) => {
+      mockNavigate(x);
+      return null;
+    }
+  };
+});
 
 describe("RestaurantEditPage tests", () => {
 
-    const queryClient = new QueryClient();
+  const axiosMock = new AxiosMockAdapter(axios);
+  let queryClient;
+
+  beforeEach(() => {
+    queryClient = new QueryClient();
+    axiosMock.reset();
+    axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+  });
+  describe("when backend doesn't return a restaurant", () => {
+    test("renders header only", async () => {
+      axiosMock.onGet("/api/Restaurant", {params: {id: 3}}).timeout();
+
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <RestaurantEditPage/>
+          </MemoryRouter>
+        </QueryClientProvider>
+      );
+
+      await screen.findByText("Edit Restaurant");
+      expect(screen.queryByTestId("RestaurantForm-name")).not.toBeInTheDocument();
+    });
+  });
+
+  describe("when backend is working", () => {
+    beforeEach(() => {
+      axiosMock.onGet("/api/Restaurant", {params: {id: 3}}).reply(200, {
+        name: "Freebirds",
+        description: "Burritos",
+        id: 3
+      });
+    });
 
     test("renders without crashing", () => {
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <RestaurantEditPage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <RestaurantEditPage/>
+          </MemoryRouter>
+        </QueryClientProvider>
+      );
     });
 
-    test("loads the correct fields", async () => {
+    test("is populated with existing restaurant", async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <RestaurantEditPage/>
+          </MemoryRouter>
+        </QueryClientProvider>
+      );
 
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <RestaurantEditPage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
+      await waitFor(() => {
+        const idField = screen.getByTestId("RestaurantForm-id");
+        expect(idField).toHaveValue("3");
+      });
 
-        expect(screen.getByTestId("RestaurantForm-name")).toBeInTheDocument();
-        expect(screen.getByDisplayValue('Freebirds')).toBeInTheDocument();
-        expect(screen.getByDisplayValue('Burritos')).toBeInTheDocument();
+      const nameInput = screen.getByLabelText("Name");
+      const descriptionInput = screen.getByLabelText("Description");
+      const updateButton = screen.getByText("Update");
+
+      expect(nameInput).toHaveValue("Freebirds");
+      expect(descriptionInput).toHaveValue("Burritos");
+      expect(updateButton).toBeInTheDocument();
     });
 
-    test("redirects to /restaurants on submit", async () => {
+    test("on submit, calls backend and redirects to index page", async () => {
+      axiosMock.onPut("/api/Restaurant").reply(200, {
+        name: "Angrybirds",
+        description: "No more burritos",
+        id: "3"
+      });
 
-        const restoreConsole = mockConsole();
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <RestaurantEditPage/>
+          </MemoryRouter>
+        </QueryClientProvider>
+      );
 
-        mockUpdate.mockReturnValue({
-            "restaurant": {
-                id: 3,
-                name: "South Coast Deli (Goleta)",
-                description: "Sandwiches, Salads and more"
-            }
-        });
+      await waitFor(async () => {
+        const idField = await screen.findByTestId("RestaurantForm-id");
+        expect(idField).toHaveValue("3");
+      });
 
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <RestaurantEditPage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        )
+      const nameInput = screen.getByLabelText("Name");
+      const descriptionInput = screen.getByLabelText("Description");
+      const updateButton = screen.getByText("Update");
 
-        const nameInput = screen.getByLabelText("Name");
-        expect(nameInput).toBeInTheDocument();
+      expect(nameInput).toHaveValue("Freebirds");
+      expect(descriptionInput).toHaveValue("Burritos");
+      expect(updateButton).toBeInTheDocument();
+
+      fireEvent.change(nameInput, {target: {value: 'Angrybirds'}});
+      fireEvent.change(descriptionInput, {target: {value: 'No more burritos'}});
+
+      fireEvent.click(updateButton);
+
+      await waitFor(() => expect(mockToast).toHaveBeenCalledWith(
+        "Restaurant Updated - id: 3 name: Angrybirds"
+      ));
+      await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith({
+        to: "/restaurants"
+      }));
 
 
-        const descriptionInput = screen.getByLabelText("Description");
-        expect(descriptionInput).toBeInTheDocument();
-
-        const updateButton = screen.getByText("Update");
-        expect(updateButton).toBeInTheDocument();
-
-    
-            fireEvent.change(nameInput, { target: { value: 'South Coast Deli (Goleta)' } })
-            fireEvent.change(descriptionInput, { target: { value: 'Sandwiches, Salads and more' } })
-            fireEvent.click(updateButton);
-   
-
-        await waitFor(() => expect(mockUpdate).toHaveBeenCalled());
-        await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith("/restaurants"));
-
-        // assert - check that the console.log was called with the expected message
-        expect(console.log).toHaveBeenCalled();
-        const message = console.log.mock.calls[0][0];
-        const expectedMessage =  `updatedRestaurant: {"restaurant":{"id":3,"name":"South Coast Deli (Goleta)","description":"Sandwiches, Salads and more"}`
-
-        expect(message).toMatch(expectedMessage);
-        restoreConsole();
-
+      expect(axiosMock.history.put.length).toBe(1);
+      expect(axiosMock.history.put[0].params).toEqual({id: 3});
+      expect(axiosMock.history.put[0].data).toBe(JSON.stringify({
+        name: "Angrybirds",
+        description: "No more burritos",
+      }));
     });
-
+  });
 });
-
-
