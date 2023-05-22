413ac9e02e438e3630012d8a2bf67b24ac903e89
Jacob Toomey <jtoomey@ucsb.edu>
2023-05-21 17:42:15 -0700

    Update RestaurantIndexPage.test.js

--- frontend/src/tests/pages/Restaurants/RestaurantIndexPage.test.js
+++ frontend/src/tests/pages/Restaurants/RestaurantIndexPage.test.js
@@ -1,54 +1,52 @@
-import { render, screen, waitFor } from "@testing-library/react";
 import RestaurantIndexPage from "main/pages/Restaurants/RestaurantIndexPage";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { render, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import mockConsole from "jest-mock-console";
-// for mocking /api/currentUser and /api/systemInfo
-import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import mockConsole from "jest-mock-console";
+import AxiosMockAdapter from "axios-mock-adapter";
+import { restaurantFixtures } from "../../../fixtures/restaurantFixtures";
 import axios from "axios";
-import AxiosMockAdapter from "axios-mock-adapter";;
-const mockNavigate = jest.fn();
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useNavigate: () => mockNavigate
-}));
 
-const mockDelete = jest.fn();
-jest.mock('main/utils/restaurantUtils', () => {
+
+
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
     return {
         __esModule: true,
-        restaurantUtils: {
-            del: (id) => {
-                return mockDelete(id);
-            },
-            get: () => {
-                return {
-                    nextId: 5,
-                    restaurants: [
-                        {
-                            "id": 3,
-                            "name": "Freebirds",
-                            "address": "879 Embarcadero del Norte",
-                            "city": "Isla Vista",
-                            "state": "CA",
-                            "zip": "93117",
-                            "description": "Burrito joint, and iconic Isla Vista location"
-                        },
-                    ]
-                }
-            }
-        }
-    }
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
 });
 
-
 describe("RestaurantIndexPage tests", () => {
-    const axiosMock =new AxiosMockAdapter(axios);
-    axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-    axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-    const queryClient = new QueryClient();
-    test("renders without crashing", () => {
+    const axiosMock = new AxiosMockAdapter(axios);
+    const testId = "RestaurantTable";
+
+    let queryClient;
+    beforeEach(() => {
+        queryClient = new QueryClient();
+    });
+
+    const setupUserOnly = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    };
+
+    const setupAdminUser = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.adminUser);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    };
+
+    test("renders without crashing for normal user", () => {
+        setupUserOnly();
+        axiosMock.onGet("/api/Restaurant/all").reply(200, []);
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -58,7 +56,22 @@ describe("RestaurantIndexPage tests", () => {
         );
     });
 
-    test("renders correct fields", () => {
+    test("renders without crashing for admin ", () => {
+        setupAdminUser();
+        axiosMock.onGet("/api/Restaurant/all").reply(200, []);
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <RestaurantIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+    });
+
+    test("renders three restaurants correctly for regular user", async () => {
+        setupUserOnly();
+        axiosMock.onGet("/api/Restaurant/all").reply(200, restaurantFixtures.threeRestaurants);
+
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -71,20 +84,23 @@ describe("RestaurantIndexPage tests", () => {
         expect(createRestaurantButton).toBeInTheDocument();
         expect(createRestaurantButton).toHaveAttribute("style", "float: right;");
 
-        const name = screen.getByText("Freebirds");
-        expect(name).toBeInTheDocument();
+        await waitFor(() => {
+            const name = screen.getByText("Freebirds");
+            expect(name).toBeInTheDocument();
+        });
 
         const description = screen.getByText("Burrito joint, and iconic Isla Vista location");
         expect(description).toBeInTheDocument();
 
-        expect(screen.getByTestId("RestaurantTable-cell-row-0-col-Delete-button")).toBeInTheDocument();
+
         expect(screen.getByTestId("RestaurantTable-cell-row-0-col-Details-button")).toBeInTheDocument();
-        expect(screen.getByTestId("RestaurantTable-cell-row-0-col-Edit-button")).toBeInTheDocument();
+        //expect(screen.queryByTestId("RestaurantTable-cell-row-0-col-Delete-button")).not.toBeInTheDocument();
+        //expect(screen.queryByTestId("RestaurantTable-cell-row-0-col-Edit-button")).not.toBeInTheDocument();
     });
 
-    test("delete button calls delete and reloads page", async () => {
-
-        const restoreConsole = mockConsole();
+    test("renders three restaurants correctly for admin user", async () => {
+        setupAdminUser();
+        axiosMock.onGet("/api/Restaurant/all").reply(200, restaurantFixtures.threeRestaurants);
 
         render(
             <QueryClientProvider client={queryClient}>
@@ -94,32 +110,42 @@ describe("RestaurantIndexPage tests", () => {
             </QueryClientProvider>
         );
 
-        const name = screen.getByText("Freebirds");
-        expect(name).toBeInTheDocument();
+        const createRestaurantButton = screen.getByText("Create Restaurant");
+        expect(createRestaurantButton).toBeInTheDocument();
+        expect(createRestaurantButton).toHaveAttribute("style", "float: right;");
+
+        await waitFor(() => {
+            const name = screen.getByText("Freebirds");
+            expect(name).toBeInTheDocument();
+        });
 
         const description = screen.getByText("Burrito joint, and iconic Isla Vista location");
         expect(description).toBeInTheDocument();
+        expect(screen.getByTestId("RestaurantTable-cell-row-0-col-Details-button")).toBeInTheDocument();
+        expect(screen.getByTestId("RestaurantTable-cell-row-0-col-Delete-button")).toBeInTheDocument();
+        expect(screen.getByTestId("RestaurantTable-cell-row-0-col-Edit-button")).toBeInTheDocument();
+    });
+    test("renders empty table when backend unavailable", async () => {
+        setupUserOnly();
+        const restoreConsole = mockConsole();
+        axiosMock.onGet("/api/Restaurant/all").timeout();
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <RestaurantIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
 
-        const deleteButton = screen.getByTestId("RestaurantTable-cell-row-0-col-Delete-button");
-        expect(deleteButton).toBeInTheDocument();
+        await waitFor(() => expect(axiosMock.history.get.length).toBeGreaterThanOrEqual(1));
 
-        deleteButton.click();
-
-        expect(mockDelete).toHaveBeenCalledTimes(1);
-        expect(mockDelete).toHaveBeenCalledWith(3);
-
-        await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith("/restaurants"));
-
-
-        // assert - check that the console.log was called with the expected message
-        expect(console.log).toHaveBeenCalled();
-        const message = console.log.mock.calls[0][0];
-        const expectedMessage = `RestaurantIndexPage deleteCallback: {"id":3,"name":"Freebirds","description":"Burrito joint, and iconic Isla Vista location"}`;
-        expect(message).toMatch(expectedMessage);
+        expect(console.error).toHaveBeenCalledWith(
+            expect.stringMatching("Error communicating with backend via GET on /api/Restaurant/all"),
+            expect.anything()
+        );
         restoreConsole();
 
+        expect(screen.queryByTestId(`${testId}-cell-row-0-id`)).not.toBeInTheDocument();
     });
 
-});;
-
-
+});
