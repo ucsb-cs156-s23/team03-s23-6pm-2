bbe42deade09a4849f0efa6f060f3c8ad2061a18
jgarcia-ucsb <114465206+jgarcia-ucsb@users.noreply.github.com>
2023-05-22 23:57:37 -0700

    Added Index and Details Pages for Books (#28)
    * Added Index and Details Pages for Books
    
    * Added Table for Books

--- frontend/src/main/pages/Books/BookDetailsPage.js
+++ frontend/src/main/pages/Books/BookDetailsPage.js
@@ -1,19 +1,30 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 import BookTable from 'main/components/Books/BookTable';
-import { bookUtils } from 'main/utils/bookUtils';
+import {useBackend} from "../../utils/useBackend";
 
 export default function BookDetailsPage() {
   let { id } = useParams();
 
-  const response = bookUtils.getById(id);
+  const { data: book } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      [`/api/books?id=${id}`],
+      {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+        method: "GET",
+        url: `/api/books`,
+        params: {id}
+      }
+    );
+
+  const books = book ? [book] : [];
 
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Book Details</h1>
-        <BookTable books={[response.book]} showButtons={false} />
+        <BookTable books={books} showButtons={false}/>
       </div>
     </BasicLayout>
-  )
+  );
 }
--- frontend/src/tests/pages/Books/BookIndexPage.test.js
+++ frontend/src/tests/pages/Books/BookIndexPage.test.js
@@ -1,51 +1,52 @@
-import { render, screen, waitFor } from "@testing-library/react";
-import BookIndexPage from "main/pages/Books/BookIndexPage";
+import { fireEvent, render, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
-import mockConsole from "jest-mock-console";
-// for mocking /api/currentUser and /api/systemInfo
-import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
-import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
-import axios from "axios";
-import AxiosMockAdapter from "axios-mock-adapter";;
-const mockNavigate = jest.fn();
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useNavigate: () => mockNavigate
-}));
+import BookIndexPage from "main/pages/Books/BookIndexPage";
 
-const mockDelete = jest.fn();
-jest.mock('main/utils/bookUtils', () => {
+
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import { bookFixtures } from "fixtures/bookFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+import mockConsole from "jest-mock-console";
+
+
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
     return {
         __esModule: true,
-        bookUtils: {
-            del: (id) => {
-                return mockDelete(id);
-            },
-            get: () => {
-                return {
-                    nextId: 5,
-                    books: [
-                        {
-                            "id": 3,
-                            "title": "The Kite Runner",
-                            "author": "Khaled Hosseini",
-                            "year": "2003"
-                        },
-                    ]
-                }
-            }
-        }
-    }
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
 });
 
-
 describe("BookIndexPage tests", () => {
+
     const axiosMock =new AxiosMockAdapter(axios);
-    axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-    axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-    const queryClient = new QueryClient();
-    test("renders without crashing", () => {
+
+    const testId = "BookTable";
+
+    const setupUserOnly = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    };
+
+    const setupAdminUser = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.adminUser);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    };
+
+    test("renders without crashing for regular user", () => {
+        setupUserOnly();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/books/all").reply(200, []);
+
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -53,9 +54,15 @@ describe("BookIndexPage tests", () => {
                 </MemoryRouter>
             </QueryClientProvider>
         );
+
+
     });
 
-    test("renders correct fields", () => {
+    test("renders without crashing for admin user", () => {
+        setupAdminUser();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/books/all").reply(200, []);
+
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -64,26 +71,56 @@ describe("BookIndexPage tests", () => {
             </QueryClientProvider>
         );
 
-        const createBookButton = screen.getByText("Create Book");
-        expect(createBookButton).toBeInTheDocument();
-        expect(createBookButton).toHaveAttribute("style", "float: right;");
 
-        const title = screen.getByText("The Kite Runner");
-        expect(title).toBeInTheDocument();
-
-        const author = screen.getByText("Khaled Hosseini");
-        expect(author).toBeInTheDocument();
-
-        expect(screen.getByTestId("BookTable-cell-row-0-col-Delete-button")).toBeInTheDocument();
-        expect(screen.getByTestId("BookTable-cell-row-0-col-Details-button")).toBeInTheDocument();
-        expect(screen.getByTestId("BookTable-cell-row-0-col-Edit-button")).toBeInTheDocument();
     });
 
-    test("delete button calls delete and reloads page", async () => {
+    test("renders three books without crashing for regular user", async () => {
+        setupUserOnly();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/books/all").reply(200, bookFixtures.threeBooks);
+
+        const { getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <BookIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("2"); });
+        expect(getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("3");
+        expect(getByTestId(`${testId}-cell-row-2-col-id`)).toHaveTextContent("4");
+
+    });
+
+    test("renders three books without crashing for admin user", async () => {
+        setupAdminUser();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/books/all").reply(200, bookFixtures.threeBooks);
+
+        const { getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <BookIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("2"); });
+        expect(getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("3");
+        expect(getByTestId(`${testId}-cell-row-2-col-id`)).toHaveTextContent("4");
+
+    });
+
+    test("renders empty table when backend unavailable, user only", async () => {
+        setupUserOnly();
+
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/books/all").timeout();
 
         const restoreConsole = mockConsole();
 
-        render(
+        const { queryByTestId } = render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
                     <BookIndexPage />
@@ -91,30 +128,45 @@ describe("BookIndexPage tests", () => {
             </QueryClientProvider>
         );
 
-        const title = screen.getByText("The Kite Runner");
-        expect(title).toBeInTheDocument();
+        await waitFor(() => { expect(axiosMock.history.get.length).toBeGreaterThanOrEqual(1); });
 
-        const author = screen.getByText("Khaled Hosseini");
-        expect(author).toBeInTheDocument();
-
-        const deleteButton = screen.getByTestId("BookTable-cell-row-0-col-Delete-button");
-        expect(deleteButton).toBeInTheDocument();
-
-        deleteButton.click();
-
-        expect(mockDelete).toHaveBeenCalledTimes(1);
-        expect(mockDelete).toHaveBeenCalledWith(3);
-
-        await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith("/books"));
-
-
-        // assert - check that the console.log was called with the expected message
-        expect(console.log).toHaveBeenCalled();
-        const message = console.log.mock.calls[0][0];
-        const expectedMessage = `BookIndexPage deleteCallback: {"id":3,"title":"The Kite Runner","author":"Khaled Hosseini","year":"2003"}`;
-        expect(message).toMatch(expectedMessage);
+        const errorMessage = console.error.mock.calls[0][0];
+        expect(errorMessage).toMatch("Error communicating with backend via GET on /api/books/all");
         restoreConsole();
 
+        expect(queryByTestId(`${testId}-cell-row-0-col-id`)).not.toBeInTheDocument();
     });
 
+    // test("what happens when you click delete, admin", async () => {
+    //     setupAdminUser();
+
+    //     const queryClient = new QueryClient();
+    //     axiosMock.onGet("/api/books/all").reply(200, bookFixtures.threeBooks);
+    //     axiosMock.onDelete("/api/books").reply(200, "Book with id 2 was deleted");
+
+
+    //     const { getByTestId } = render(
+    //         <QueryClientProvider client={queryClient}>
+    //             <MemoryRouter>
+    //                 <BookIndexPage />
+    //             </MemoryRouter>
+    //         </QueryClientProvider>
+    //     );
+
+    //     await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toBeInTheDocument(); });
+
+    //    expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("2"); 
+
+
+    //     const deleteButton = getByTestId(`${testId}-cell-row-0-col-Delete-button`);
+    //     expect(deleteButton).toBeInTheDocument();
+       
+    //     fireEvent.click(deleteButton);
+
+    //     await waitFor(() => { expect(mockToast).toBeCalledWith("Book with id 2 was deleted") });
+
+    // });
+
 });
+
+
