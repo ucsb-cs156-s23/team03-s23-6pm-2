8dca8a04c9785559994cdaa53c464247bab4c2b7
Christopher Chang <51393127+chriscerie@users.noreply.github.com>
2023-05-22 18:26:35 -0700

    Update DogTable and DogIndexPage

--- frontend/src/main/pages/Dogs/DogIndexPage.js
+++ frontend/src/main/pages/Dogs/DogIndexPage.js
@@ -2,32 +2,30 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import DogTable from 'main/components/Dogs/DogTable';
-import { dogUtils } from 'main/utils/dogUtils';
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useBackend } from 'main/utils/useBackend';
+import { useCurrentUser } from 'main/utils/currentUser';
 
 export default function DogIndexPage() {
-    const navigate = useNavigate();
+  // Stryker disable all : don't test internal caching of React Query
+  const { data: dogs } = useBackend(
+  
+    ["Get all dogs"],
+    {method: "GET", url: "/api/dogs/all"},
+    []
+  );;
+  // Stryker enable all
+  const currentUser = useCurrentUser();
 
-    const dogCollection = dogUtils.get();
-    const dogs = dogCollection.dogs;
-
-    const showCell = (cell) => JSON.stringify(cell.row.values);
-
-    const deleteCallback = async (cell) => {
-        console.log(`DogIndexPage deleteCallback: ${showCell(cell)})`);
-        dogUtils.del(cell.row.values.id);
-        navigate("/dogs");
-    }
-
-    return (
-        <BasicLayout>
-            <div className="pt-2">
-                <Button style={{ float: "right" }} as={Link} to="/dogs/create">
-                    Create Dog
-                </Button>
-                <h1>Dogs</h1>
-                <DogTable dogs={dogs} deleteCallback={deleteCallback} />
-            </div>
-        </BasicLayout>
-    )
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <Button style={{ float: "right" }} as={Link} to="/dogs/create">
+          Create Dog
+        </Button>
+        <h1>Dogs</h1>
+        <DogTable dogs={dogs} currentUser={currentUser} />
+      </div>
+    </BasicLayout>
+  )
 }
--- frontend/src/tests/components/Dogs/DogTable.test.js
+++ frontend/src/tests/components/Dogs/DogTable.test.js
@@ -1,8 +1,11 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import DogTable, { showCell } from "main/components/Dogs/DogTable";
-import { dogFixtures } from "fixtures/dogFixtures";
+import {fireEvent, render, screen, waitFor} from "@testing-library/react";
+import {MemoryRouter} from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "react-query";
+import DogTable from "main/components/Dogs/DogTable";
+import {dogFixtures} from "fixtures/dogFixtures";
+import {currentUserFixtures} from "../../../fixtures/currentUserFixtures";
+import AxiosMockAdapter from "axios-mock-adapter";
+import axios from "axios";
 import mockConsole from "jest-mock-console";
 
 const mockedNavigate = jest.fn();
@@ -12,202 +15,186 @@ jest.mock('react-router-dom', () => ({
   useNavigate: () => mockedNavigate
 }));
 
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+  const originalModule = jest.requireActual('react-toastify');
+  return {
+    __esModule: true,
+    ...originalModule,
+    toast: (x) => mockToast(x)
+  };
+});
+
 describe("DogTable tests", () => {
   const queryClient = new QueryClient();
-
-  const expectedHeaders = ["id", "Name", "Breed"];
-  const expectedFields = ["id", "name", "breed"];
+  const expectedHeaders = ["Name", "Breed"];
+  const expectedFields = ["name", "breed"];
   const testId = "DogTable";
 
-  test("showCell function works properly", () => {
-    const cell = {
-      row: {
-        values: { a: 1, b: 2, c: 3 }
-      },
-    };
-    expect(showCell(cell)).toBe(`{"a":1,"b":2,"c":3}`);
-  });
-
-  test("renders without crashing for empty table", () => {
-    render(
+  const renderTable = (dogs, currentUser) => {
+    return render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
-          <DogTable dogs={[]} />
+          <DogTable dogs={dogs} currentUser={currentUser} />
         </MemoryRouter>
       </QueryClientProvider>
     );
+  };
+
+  test("renders without crashing for empty table with user not logged in", () => {
+    renderTable([], null);
   });
 
+  test("renders without crashing for empty table for ordinary user", () => {
+    const currentUser = currentUserFixtures.userOnly;
+    renderTable([], currentUser);
+  });
+
+  test("renders without crashing for empty table for admin", () => {
+    const currentUser = currentUserFixtures.adminUser;
+    renderTable([], currentUser);
+  });
+
+  test("Has the expected column headers, content, and buttons for admin user", () => {
+     const currentUser = currentUserFixtures.adminUser;
+ 
+     render(
+       <QueryClientProvider client={queryClient}>
+         <MemoryRouter>
+           <DogTable dogs={dogFixtures.threeDogs} currentUser={currentUser}/>
+         </MemoryRouter>
+       </QueryClientProvider>
+     );
+ 
+     expectedHeaders.forEach((headerText) => {
+       const header = screen.getByText(headerText);
+       expect(header).toBeInTheDocument();
+     });
+ 
+     expectedFields.forEach((field) => {
+       const header = screen.getByTestId(`${testId}-cell-row-0-col-${field}`);
+       expect(header).toBeInTheDocument();
+     });
+ 
+     expect(screen.getByTestId(`${testId}-cell-row-0-col-name`)).toHaveTextContent("Annie");
+     expect(screen.getByTestId(`${testId}-cell-row-0-col-breed`)).toHaveTextContent("Poodle");
+ 
+     expect(screen.getByTestId(`${testId}-cell-row-1-col-name`)).toHaveTextContent("Tom");
+     expect(screen.getByTestId(`${testId}-cell-row-1-col-breed`)).toHaveTextContent("German Shepherd");
+ 
+     const detailsButton = screen.getByTestId(`${testId}-cell-row-0-col-Details-button`);
+     expect(detailsButton).toBeInTheDocument();
+     expect(detailsButton).toHaveClass("btn-primary");
+ 
+     const editButton = screen.getByTestId(`${testId}-cell-row-0-col-Edit-button`);
+     expect(editButton).toBeInTheDocument();
+     expect(editButton).toHaveClass("btn-primary");
+ 
+     const deleteButton = screen.getByTestId(`${testId}-cell-row-0-col-Delete-button`);
+     expect(deleteButton).toBeInTheDocument();
+     expect(deleteButton).toHaveClass("btn-danger");
+   });
+ 
+ 
+   test("Has the expected column headers, content and no buttons when showButtons=false", () => {
+     const currentUser = currentUserFixtures.adminUser;
+     render(
+       <QueryClientProvider client={queryClient}>
+         <MemoryRouter>
+           <DogTable dogs={dogFixtures.threeDogs}
+                            showButtons={false}
+                            currentUser={currentUser}/>
+         </MemoryRouter>
+       </QueryClientProvider>
+     );
+ 
+     expectedHeaders.forEach((headerText) => {
+       const header = screen.getByText(headerText);
+       expect(header).toBeInTheDocument();
+     });
+ 
+     expectedFields.forEach((field) => {
+       const header = screen.getByTestId(`${testId}-cell-row-0-col-${field}`);
+       expect(header).toBeInTheDocument();
+     });
+ 
+     expect(screen.getByTestId(`${testId}-cell-row-0-col-name`)).toHaveTextContent("Annie");
+     expect(screen.getByTestId(`${testId}-cell-row-0-col-breed`)).toHaveTextContent("Poodle");
+ 
+     expect(screen.getByTestId(`${testId}-cell-row-1-col-name`)).toHaveTextContent("Tom");
+     expect(screen.getByTestId(`${testId}-cell-row-1-col-breed`)).toHaveTextContent("German Shepherd");
+ 
+     expect(screen.queryByText("Delete")).not.toBeInTheDocument();
+     expect(screen.queryByText("Edit")).not.toBeInTheDocument();
+     expect(screen.queryByText("Details")).not.toBeInTheDocument();
+   });
+
+ 
 
 
-  test("Has the expected column headers, content and buttons", () => {
-
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <DogTable dogs={dogFixtures.threeDogs} />
-        </MemoryRouter>
-      </QueryClientProvider>
-    );
-
-    expectedHeaders.forEach((headerText) => {
-      const header = screen.getByText(headerText);
-      expect(header).toBeInTheDocument();
-    });
-
-    expectedFields.forEach((field) => {
-      const header = screen.getByTestId(`${testId}-cell-row-0-col-${field}`);
-      expect(header).toBeInTheDocument();
-    });
-
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("2");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-name`)).toHaveTextContent("Annie");
-
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("3");
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-name`)).toHaveTextContent("Tom");
-
-    const detailsButton = screen.getByTestId(`${testId}-cell-row-0-col-Details-button`);
-    expect(detailsButton).toBeInTheDocument();
-    expect(detailsButton).toHaveClass("btn-primary");
-
+   test("Edit button navigates to the edit page for admin user", async () => {
+    const currentUser = currentUserFixtures.adminUser;
+    const dogs = dogFixtures.threeDogs;
+  
+    renderTable(dogs, currentUser);
+  
+    expect(await screen.findByTestId(`${testId}-cell-row-0-col-name`)).toHaveTextContent("Annie");
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-breed`)).toHaveTextContent("Poodle");
+  
     const editButton = screen.getByTestId(`${testId}-cell-row-0-col-Edit-button`);
     expect(editButton).toBeInTheDocument();
-    expect(editButton).toHaveClass("btn-primary");
-
-    const deleteButton = screen.getByTestId(`${testId}-cell-row-0-col-Delete-button`);
-    expect(deleteButton).toBeInTheDocument();
-    expect(deleteButton).toHaveClass("btn-danger");
-
-  });
-
-  test("Has the expected column headers, content and no buttons when showButtons=false", () => {
-
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <DogTable dogs={dogFixtures.threeDogs} showButtons={false} />
-        </MemoryRouter>
-      </QueryClientProvider>
-    );
-
-    expectedHeaders.forEach((headerText) => {
-      const header = screen.getByText(headerText);
-      expect(header).toBeInTheDocument();
-    });
-
-    expectedFields.forEach((field) => {
-      const header = screen.getByTestId(`${testId}-cell-row-0-col-${field}`);
-      expect(header).toBeInTheDocument();
-    });
-
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("2");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-name`)).toHaveTextContent("Annie");
-
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("3");
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-name`)).toHaveTextContent("Tom");
-
-    expect(screen.queryByText("Delete")).not.toBeInTheDocument();
-    expect(screen.queryByText("Edit")).not.toBeInTheDocument();
-    expect(screen.queryByText("Details")).not.toBeInTheDocument();
-  });
-
-
-  test("Edit button navigates to the edit page", async () => {
-    // arrange
-    const restoreConsole = mockConsole();
-
-    // act - render the component
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <DogTable dogs={dogFixtures.threeDogs} />
-        </MemoryRouter>
-      </QueryClientProvider>
-    );
-
-    // assert - check that the expected content is rendered
-    expect(await screen.findByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("2");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-name`)).toHaveTextContent("Annie");
-
-    const editButton = screen.getByTestId(`${testId}-cell-row-0-col-Edit-button`);
-    expect(editButton).toBeInTheDocument();
-
-    // act - click the edit button
+  
     fireEvent.click(editButton);
-
-    // assert - check that the navigate function was called with the expected path
-    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith('/dogs/edit/2'));
-
-    // assert - check that the console.log was called with the expected message
-    expect(console.log).toHaveBeenCalled();
-    const message = console.log.mock.calls[0][0];
-    const expectedMessage = `editCallback: {"id":2,"name":"Annie","breed":"Poodle"})`;
-    expect(message).toMatch(expectedMessage);
-    restoreConsole();
+  
+    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith('/dogs/edit/Annie'));
   });
 
   test("Details button navigates to the details page", async () => {
-    // arrange
-    const restoreConsole = mockConsole();
-
-    // act - render the component
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <DogTable dogs={dogFixtures.threeDogs} />
-        </MemoryRouter>
-      </QueryClientProvider>
-    );
-
-    // assert - check that the expected content is rendered
-    expect(await screen.findByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("2");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-name`)).toHaveTextContent("Annie");
-
+    const dogs = dogFixtures.threeDogs;
+  
+    renderTable(dogs);
+  
+    expect(await screen.findByTestId(`${testId}-cell-row-0-col-name`)).toHaveTextContent("Annie");
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-breed`)).toHaveTextContent("Poodle");
+  
     const detailsButton = screen.getByTestId(`${testId}-cell-row-0-col-Details-button`);
     expect(detailsButton).toBeInTheDocument();
-
-    // act - click the details button
+  
     fireEvent.click(detailsButton);
-
-    // assert - check that the navigate function was called with the expected path
-    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith('/dogs/details/2'));
-
-    // assert - check that the console.log was called with the expected message
-    expect(console.log).toHaveBeenCalled();
-    const message = console.log.mock.calls[0][0];
-    const expectedMessage = `detailsCallback: {"id":2,"name":"Annie","breed":"Poodle"})`;
-    expect(message).toMatch(expectedMessage);
-    restoreConsole();
+  
+    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith('/dogs/details/Annie'));
   });
 
+  const axiosMock = new AxiosMockAdapter(axios);
+
   test("Delete button calls delete callback", async () => {
-    // arrange
     const restoreConsole = mockConsole();
+    axiosMock.onDelete('/api/dogs', {params: {name: "Annie"}}).reply(200, "Dog with name Annie was deleted");
 
-    // act - render the component
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <DogTable dogs={dogFixtures.threeDogs} />
-        </MemoryRouter>
-      </QueryClientProvider>
-    );
-
-    // assert - check that the expected content is rendered
-    expect(await screen.findByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("2");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-name`)).toHaveTextContent("Annie");
-
+    const currentUser = currentUserFixtures.adminUser;
+    const dogs = dogFixtures.threeDogs;
+  
+    renderTable(dogs, currentUser);
+  
+    expect(await screen.findByTestId(`${testId}-cell-row-0-col-name`)).toHaveTextContent("Annie");
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-breed`)).toHaveTextContent("Poodle");
+  
     const deleteButton = screen.getByTestId(`${testId}-cell-row-0-col-Delete-button`);
     expect(deleteButton).toBeInTheDocument();
-
-     // act - click the delete button
+  
     fireEvent.click(deleteButton);
-
-     // assert - check that the console.log was called with the expected message
-     await(waitFor(() => expect(console.log).toHaveBeenCalled()));
-     const message = console.log.mock.calls[0][0];
-     const expectedMessage = `deleteCallback: {"id":2,"name":"Annie","breed":"Poodle"})`;
-     expect(message).toMatch(expectedMessage);
-     restoreConsole();
+  
+    await waitFor(() => {
+      expect(axiosMock.history.delete.length).toBe(1);
+    });
+    
+    await waitFor(() => {
+      expect(axiosMock.history.delete[0].url).toBe('/api/dogs');
+    });
+  
+    expect(console.log).toHaveBeenCalled();
+    expect(mockToast).toHaveBeenCalledWith("Dog with name Annie was deleted");
+  
+    restoreConsole();
   });
 });
