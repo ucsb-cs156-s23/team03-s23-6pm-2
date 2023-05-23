bbe58aaeaab1c318255297ede5eaf2d8c15d99f4
Rylan Pow <rylan@ucsb.edu>
2023-05-22 19:36:09 -0700

    book create and edit pages + local tests, all pass

--- frontend/src/main/pages/Books/BookEditPage.js
+++ frontend/src/main/pages/Books/BookEditPage.js
@@ -2,27 +2,68 @@ import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import { useParams } from "react-router-dom";
 import { bookUtils }  from 'main/utils/bookUtils';
 import BookForm from 'main/components/Books/BookForm';
-import { useNavigate } from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
 
 export default function BookEditPage() {
     let { id } = useParams();
 
-    let navigate = useNavigate(); 
+    const { data: book, error, status } =
+        useBackend(
+            // Stryker disable next-line all : don't test internal caching of React Query
+            [`/api/books?id=${id}`],
+            {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+                method: "GET",
+                url: `/api/books`,
+                params: {
+                    id
+                }
+            }
+        );
 
-    const response = bookUtils.getById(id);
+    const objectToAxiosPutParams = (book) => ({
+        url: "/api/books",
+        method: "PUT",
+        params: {
+            id: book.id,
+        },
+        data: {
+            title: book.title,
+            author: book.author,
+            year: book.year
+        }
+    });
 
-    const onSubmit = async (book) => {
-        const updatedBook = bookUtils.update(book);
-        console.log("updatedBook: " + JSON.stringify(updatedBook));
-        navigate("/books");
-    }  
+    const onSuccess = (book) => {
+        toast(`Book Updated - id: ${book.id} title: ${book.title}`);
+    }
+
+    const mutation = useBackendMutation(
+        objectToAxiosPutParams,
+        { onSuccess },
+        // Stryker disable next-line all : hard to set up test for caching
+        [`/api/books?id=${id}`]
+    );
+
+    const { isSuccess } = mutation
+
+    const onSubmit = async (data) => {
+        mutation.mutate(data);
+    }
+
+    if (isSuccess) {
+        return <Navigate to="/books/list" />
+    }
 
     return (
         <BasicLayout>
             <div className="pt-2">
                 <h1>Edit Book</h1>
-                <BookForm submitAction={onSubmit} buttonLabel={"Update"} initialContents={response.book}/>
+                {book &&
+                    <BookForm initialContents={book} submitAction={onSubmit} buttonLabel="Update" />
+                }
             </div>
         </BasicLayout>
     )
